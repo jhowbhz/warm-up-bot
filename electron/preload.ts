d550d05cad868647a236e7445449a6dae6d9fc54
Jhon <31408451+jhowbhz@users.noreply.github.com>
2026-02-18 22:17:43 -0300

    remove: not used folder

--- electron/preload.ts
+++
@@ -1,7 +0,0 @@
-import { contextBridge } from 'electron';
-
-// Expor APIs seguras para o renderer
-contextBridge.exposeInMainWorld('electron', {
-  // Podemos adicionar funções IPC aqui se necessário no futuro
-  platform: process.platform,
-});
